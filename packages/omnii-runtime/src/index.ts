291eae2a2655282e2135c1e4dc218b62024c0738
carbonactual <carbonactuall@gmail.com>
2026-08-20 22:26:12 +0100

    feat: export naming and multi-species runtimes

--- packages/omnii-runtime/src/index.ts
+++ packages/omnii-runtime/src/index.ts
@@ -34,3 +34,5 @@ export * from "./causality-runtime";
 export * from "./scenario-runtime";
 export * from "./quality-runtime";
 export * from "./economic-composition-runtime";
+export * from "./naming-resolution-runtime";
+export * from "./multi-species-runtime";

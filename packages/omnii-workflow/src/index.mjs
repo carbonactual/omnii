64f48a83f08cf194637d2efa569e36cc0806d162
carbonactual <carbonactuall@gmail.com>
2026-09-05 11:56:38 +0100

    feat(omnii-workflow): expose canonical workflow fabric

---
+++ packages/omnii-workflow/src/index.mjs
@@ -0,0 +1,6 @@
+export * from './forms.mjs';
+export * from './process.mjs';
+export * from './tasks.mjs';
+export * from './workers.mjs';
+export * from './automation.mjs';
+export * from './registry.mjs';

d658e2ed926eee9f75bbe75a3a703091cedcb052
carbonactual <carbonactuall@gmail.com>
2026-09-06 05:56:55 +0100

    feat: export InstituteGPT canonical contracts

---
+++ packages/institutegpt/src/index.mjs
@@ -0,0 +1,8 @@
+export {
+  LEARNING_MODES,
+  SUPERVISION_LEVELS,
+  createLearningIntent,
+  createLearningRecord,
+  createAiLearningRecord,
+  classifyCredentialAuthority,
+} from './contracts.mjs';

b999dbb1684062e700b595fadbbee36b0a0e0b41
carbonactual <carbonactuall@gmail.com>
2026-09-08 01:43:27 +0100

    feat: export mission learning runtime

--- packages/omnii-runtime/src/index.ts
+++ packages/omnii-runtime/src/index.ts
@@ -66,6 +66,7 @@ export * from "./process-worker-runtime";
 export * from "./management-runtime";
 export * from "./mission-intelligence-runtime";
 export * from "./mission-adaptation-runtime";
+export * from "./mission-learning-runtime";
 export * from "./admission-runtime";
 export * from "./bunk-property-ontology";
 export * from "./bunk-property-lifecycle";

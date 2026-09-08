31af4db829ce9b16f81433acd25572fc7668ec82
carbonactual <carbonactuall@gmail.com>
2026-09-08 01:29:14 +0100

    feat: export admission runtime

--- packages/omnii-runtime/src/index.ts
+++ packages/omnii-runtime/src/index.ts
@@ -65,6 +65,7 @@ export * from "./process-progression-runtime";
 export * from "./process-worker-runtime";
 export * from "./management-runtime";
 export * from "./mission-intelligence-runtime";
+export * from "./admission-runtime";
 export * from "./bunk-property-ontology";
 export * from "./bunk-property-lifecycle";
 export * from "./bunk-economic-boundary";
